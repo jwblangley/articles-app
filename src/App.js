0e8379d5e3b459e57101d22be855ebe285adbd42
James <jwblangley@gmail.com>
2019-03-21 22:02:26 +0000

    Implement App class to render article titles
    Create Article class for each article and map the data to this class.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Load in articles JSON
+import data from './articles.json'
+
 class App extends Component {
+  // Map article data to components.
+  render() {
+    // const articles =
+    return (
+      data.results.map(article => <Article article={article} key={article.url}/>)
+    );
+  }
+}
+
+class Article extends Component {
   render() {
     return (
-      <div className="App">
-      Test
+      <div>
+        <h1>{this.props.article.title}</h1>
+        <hr/>
       </div>
     );
   }
