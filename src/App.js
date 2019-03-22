e81d67c34c413301411e2fb005ce17bc1e3373f4
James <jwblangley@gmail.com>
2019-03-22 17:23:37 +0000

    Implement filtering of articles
    Use a Set to remove duplicates from the "section" part of the data then filter on this during the functional manipulation.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,61 @@ import './App.css';
 import data from './articles.json'
 
 class App extends Component {
-  // Map article data to components.
+  constructor(props) {
+    super(props);
+    this.state = {
+      sectionFilter: ""
+    }
+  }
+
   render() {
-    // const articles =
+    /*
+    Convert the sections to a set to remove duplicates and back to an array for usage
+    N.B calling this every render does not affect performance due to React data diff.
+    */
+    const uniqueSections = [...new Set(data.results.map(article => article.section))];
+
     return (
-      data.results.map(article => <Article article={article} key={article.url}/>)
+      // sectionFilter is part of the state of App
+      <div>
+        <Selector
+          name={"Section"}
+          value={this.state.sectionFilter}
+          values={uniqueSections}
+          onSelect={(value) => this.setState({sectionFilter:value})}
+        />
+
+        {/*
+          Map data to Articles.
+          First filter to match sectionFilter (or all if none selected).
+          */}
+        {data.results
+          .filter(article => this.state.sectionFilter === ""
+            || article.section === this.state.sectionFilter)
+          .map(article => <Article article={article} key={article.url} />)}
+      </div>
     );
+  }
+}
+
+class Selector extends Component {
+  render() {
+    return (
+      <span>
+        {this.props.name}:
+        <select id='section-select'
+          value={this.props.value}
+          onChange={e => this.props.onSelect(e.target.value)}
+        >
+        <option value="">---Select a filter---</option>
+        {
+          this.props.values.map(value =>
+            <option value={value} key={value}>{value}</option>
+          )
+        }
+        </select>
+      </span>
+    )
   }
 }
 
@@ -18,10 +67,12 @@ class Article extends Component {
   render() {
     return (
       <div>
-        <h1>{this.props.article.title}</h1>
+        <a href={this.props.article.url} target="_blank"><h1>{this.props.article.title}</h1></a>
+        <h3>{this.props.article.section}</h3>
+        <img src={this.props.article.thumbnail_url} />
         <hr/>
       </div>
-    );
+    )
   }
 }
 
